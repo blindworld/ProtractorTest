457afad4ac9f6a642e86a5b79cab9c18b5ca252c
blindworld <blindworld@gmail.com>
2014-04-30 19:49:50 -0400

    removed extra references in files

---
+++ README.md
@@ -0,0 +1,4 @@
+ProtractorTest
+==============
+
+Demonstrates a bug in angular.js when navigating through $location.path('/url') while using URL based basic auth.
--- app/test/test.js
+++ app/test/test.js
@@ -1,14 +1,13 @@
 describe('test', function() {
     it('should navigate', function() {
 
-        browser.get('http://localhost:8080');
-
-        console.log('running test');
+        // browser.get('http://localhost:8080');
+        // THE NEXT LINE WILL RESULT AN INFINITE DIGEST LOOP!
+        browser.get('http://user1@localhost:8080');
 
         element(by.buttonText('Go to 2')).click();
         element(by.buttonText('Go to 1')).click();
         element(by.buttonText('Go to 2')).click();
         element(by.buttonText('Go to 1')).click();
-        browser.sleep(1000);
     });
 });
